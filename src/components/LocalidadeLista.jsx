109e8e90d0d8a3f7255c89a68f29357837ef7973
lucas <lucasfsouza2010@hotmail.com>
2025-04-12 15:07:43 -0300

    style(p1): estiliza os cards de localidade

--- src/components/LocalidadeLista.jsx
+++ src/components/LocalidadeLista.jsx
@@ -4,14 +4,27 @@ function LocalidadeLista({ localidades }) {
   return (
     <div>
       {localidades.map((localidade, index) => (
-        <Card key={index} title={`CEP: ${localidade.cep}`}>
-          <p><strong>Rua:</strong> {localidade.rua}</p>
-          <p><strong>Bairro:</strong> {localidade.bairro}</p>
-          <p><strong>Cidade:</strong> {localidade.cidade}</p>
-        </Card>
+        <Card key={index}>
+          <div style={styles.card}>
+          <p><strong>CEP:</strong> {localidade.cep}</p>
+            <p><strong>Rua:</strong> {localidade.rua}</p>
+            <p><strong>Bairro:</strong> {localidade.bairro}</p>
+            <p><strong>Cidade:</strong> {localidade.cidade}</p>
+          </div>
+          </Card>
       ))}
     </div>
   );
 }
+
+const styles = {
+  card: {
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    padding: "15px",
+    textAlign: "center",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  },
+};
 
 export default LocalidadeLista;
